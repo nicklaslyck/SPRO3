33176c65020d8b3d5a869151ef4daa33c7ada3af
nicklaslyck <nicklaslyck@gmail.com>
2019-12-11 16:59:22 +0100

    Update main.py

--- main.py
+++ main.py
@@ -45,7 +45,7 @@ state = 0
 def arduinoCallback1(channel):
     global state
     print("interrupt triggered...")
-    print(ser.read())
+    print(char(ser.read()))
     #if ser.read() == int(1):
         #state = 1
         #print("state set to 1")
