912082bbe6039fde99e3b1bd3cb8ac10d5491338
nicklaslyck <nicklaslyck@gmail.com>
2019-12-15 23:14:02 +0100

    Update main.py

--- main.py
+++ main.py
@@ -360,7 +360,8 @@ while True:
             #ser.write(chr(int(0)).encode())
             time.sleep(1)
             if count == 1:
-                ser.write(chr(int(55)).encode())
+                ser.write(chr(int(120)).encode())
+                time.sleep(1)
                 print("lowering lift and sleeping for 12s")
                 #time.sleep(12)
                 count = 0
